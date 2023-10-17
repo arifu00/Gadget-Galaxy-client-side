c57a61303618c2c4dd3b179ad4be82501f01ac21
arifu00 <arifu5758@gmail.com>
2023-10-17 19:58:27 +0600

    create project folder & set-up basic file

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-center font-black">asci w8</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      fontRoboto: ["Roboto Slab", "serif"],
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["cupcake", "acid", "cmyk"],
+  },
+};
